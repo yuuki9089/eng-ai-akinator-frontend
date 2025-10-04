5d21d258af66063d74735ed4c86a5d26c18bfad3
yuuki9089 <yuukiou90@gmail.com>
2025-10-05 02:43:06 +0900

    Add: AIからの回答をAIHintにはめる処理

--- components/EngAiAkinator.jsx
+++ components/EngAiAkinator.jsx
@@ -1,6 +1,109 @@
-import React from "react";
+import React, { useRef } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function EngAiAkinator() {
+
+  const [genres, setGenres] = useState([]);
+  const [characters, setCharacters] = useState([]);
+  const [rand_questions, setRandQuestions] = useState([]);
+  const [ai_answer, setAiAnswer] = useState("質問をしてください");
+  const session_id = useRef(null);
+  const [user_question, setUserQuestion] = useState("");
+
+  /**
+   * APIからジャンルを取得する関数
+   */
+  const selectGenres = async () => {
+    try {
+      console.log("Fetching genres...");
+      const resp = await axios.get("http://127.0.0.1:8000/genres");
+      setGenres(resp.data);
+      console.log("genres Fetched...");
+    }
+    catch (ex) {
+      console.error("Error select genres:", ex);
+    }
+  }
+  /**
+   * APIからキャラクタを取得する関数
+   */
+  const selectCharacters = async () => {
+    try {
+      console.log("Fetching characters...");
+      const resp = await axios.get("http://127.0.0.1:8000/characters");
+      setCharacters(resp.data);
+      console.log("characters Fetched...");
+    }
+    catch (ex) {
+      console.error("Error select characters:", ex);
+    }
+  }
+
+  /***
+   * 【初期化処理】
+   * DBに新規お題やセッションIDを追加
+   */
+  const initializeQuestion = async () => {
+    try {
+      console.log("initializing new Question...")
+      const resp = await axios.post("http://127.0.0.1:8000/new_question");
+      session_id.current = resp.data.session_id
+
+      console.log("initialized new Quesiton!")
+    }
+    catch (ex) {
+      console.error("Error initialize new Question:", ex)
+    }
+  }
+
+  /***
+   * 3つの質問を更新する関数
+   */
+  const selectRandQuestions = async () => {
+    try {
+      console.log("Fetching random_questions...");
+      const resp = await axios.get("http://127.0.0.1:8000/random_questions");
+      setRandQuestions(resp.data);
+      console.log("random_questions Fetched...");
+    }
+    catch (ex) {
+      console.error("Error select random_questions:", ex);
+    }
+  }
+
+  /***
+   * 質問をpostする関数
+   */
+  const registerQuestion = async (user_question) => {
+    try {
+      console.log("posting question...")
+      const data = { session_id: session_id.current, user_question_content: user_question }
+      const resp = await axios.post("http://127.0.0.1:8000/ask_ai", data);
+      setAiAnswer(resp.data.ai_answer)
+      console.log("posted question!")
+    }
+    catch (ex) {
+      console.error("Error select random_questions:", ex);
+    }
+  }
+
+  /***
+   * 質問ボタンを押下時にYour Questionに値をはめる関数
+   */
+  const registerYourQuestion = async (user_question_content) => {
+    setUserQuestion(user_question_content);
+  }
+
+  // コンストラクタ
+  useEffect(() => {
+    initializeQuestion();
+    selectGenres();
+    selectCharacters();
+    selectRandQuestions();
+  }, []
+  );
+
   return (
     <div className="h-screen w-screen bg-slate-900 text-gray-200 flex flex-col">
       {/* Header */}
@@ -21,13 +124,24 @@ export default function EngAiAkinator() {
                 <h2 className="text-blue-400 font-bold  text-4xl">
                   Ask Next Question
                 </h2>
-                <button className="text-cyan-400 text-3xl">🔄</button>
+                <button className="text-cyan-400 text-3xl" onClick={selectRandQuestions}>🔄</button>
               </div>
             </div>
             <div className="flex flex-1 gap-3 mt-2">
-              <div className="flex-1 h-full bg-slate-700 rounded-lg"></div>
-              <div className="flex-1 h-full bg-slate-700 rounded-lg"></div>
-              <div className="flex-1 h-full bg-slate-700 rounded-lg"></div>
+              {rand_questions.map((question, index) => {
+                return (
+                  <React.Fragment key={question.question_id}>
+                    <button name={index} className='flex-1 h-full bg-slate-700 rounded-lg text-2xl' value={question.question_id} onClick={
+                      () => {
+                        registerQuestion(question.question_content)
+                        registerYourQuestion(question.question_content)
+                        selectRandQuestions()
+                      }}>{question.question_content}</button>
+                  </React.Fragment>
+                )
+              }
+              )
+              }
             </div>
 
           </div>
@@ -45,8 +159,12 @@ export default function EngAiAkinator() {
             </div>
             {/*箱 */}
             <div className="flex flex-1 gap-3 mt-2">
-              <div className="flex-1 h-full bg-slate-700 rounded-lg "></div>
-              <div className="flex-1 h-full bg-slate-700 rounded-lg"></div>
+              <div className="flex-1 h-full bg-slate-700 rounded-lg text-2xl flex items-center justify-center ">
+                {user_question}
+              </div>
+              <div className="flex-1 h-full bg-slate-700 rounded-lg text-2xl flex items-center justify-center ">
+                {ai_answer}
+              </div>
             </div>
           </div>
           {/* Bottom 1/3 : Features */}
@@ -77,14 +195,38 @@ export default function EngAiAkinator() {
             <form className="flex flex-col gap-2 mt-2">
               <div className="flex flex-col">
                 <label>Genre</label>
-                <select className="bg-slate-700 p-2 rounded-lg">
-                  <option>Select Genre</option>
+                <select className="bg-slate-700 p-2 rounded-lg" name="pref" defaultValue="" required>
+                  {/* <option>Select Genre</option> */}
+                  <option value="" disabled hidden>
+                    ジャンル名を選択してください
+                  </option>
+
+                  {genres.map((genre) => {
+                    return (
+                      <React.Fragment key={genre.genre_code}>
+                        <option value={genre.genre_code}>{genre.genre_name}</option>
+                      </React.Fragment>
+                    )
+                  }
+                  )
+                  }
                 </select>
               </div>
               <div className="flex flex-col">
                 <label>Character</label>
-                <select className="bg-slate-700 p-2 rounded-lg">
-                  <option>Select Character</option>
+                <select className="bg-slate-700 p-2 rounded-lg" name="pref" defaultValue="" required>
+                  <option value="" disabled hidden>
+                    キャラクタ名を選択してください
+                  </option>
+                  {characters.map((character) => {
+                    return (
+                      <React.Fragment key={character.id}>
+                        <option value={character.id}>{character.character_name}</option>
+                      </React.Fragment>
+                    )
+                  }
+                  )
+                  }
                 </select>
 
               </div>
